a90dddbbf0683a8d1cf0c406156966733f28da37
Simon Legner <Simon.Legner@gmail.com>
2020-10-17 23:02:44 +0200

    +OpenStreetMap +Orthofoto Tirol

--- app.ts
+++ app.ts
@@ -11,6 +11,7 @@ const map = L.map(document.getElementById("map"), {
   zoom: 15,
   attributionControl: false,
 });
+const layers = L.control.layers().addTo(map);
 L.control
   .attribution({
     prefix: `
@@ -18,7 +19,34 @@ L.control
       (<a href="https://github.com/simon04/zeitlinie.tmb.at/blob/master/LICENSE">GPL v3</a>)`,
   })
   .addTo(map);
-L.tileLayer.provider("BasemapAT.grau").addTo(map);
+const basemap = L.tileLayer.provider("BasemapAT.grau").addTo(map);
+layers.addBaseLayer(basemap, "basemap.at");
+layers.addBaseLayer(L.tileLayer.provider("OpenStreetMap"), "OpenStreetMap");
+[
+  { id: "Image_1940", title: "Orthofoto Tirol: 1940" },
+  { id: "Image_1970-1982", title: "Orthofoto Tirol: 1970–1982" },
+  { id: "Image_1999-2004", title: "Orthofoto Tirol: 1999–2004" },
+  { id: "Image_2004-2009", title: "Orthofoto Tirol: 2004–2009" },
+  { id: "Image_2009-2012", title: "Orthofoto Tirol: 2009–2012" },
+  { id: "Image_2013-2015", title: "Orthofoto Tirol: 2013–2015" },
+  { id: "Image_2016", title: "Orthofoto Tirol: 2016" },
+  { id: "Image_Aktuell_RGB", title: "Orthofoto Tirol: aktuell" },
+].forEach(({ id, title }) =>
+  layers.addBaseLayer(
+    L.tileLayer.wms(
+      "https://gis.tirol.gv.at/arcgis/services/Service_Public/orthofoto/MapServer/WMSServer",
+      {
+        layers: id,
+        format: "image/jpeg",
+        maxZoom: 20,
+        attribution: `
+          <a href="https://www.tirol.gv.at/data/">data.tirol.gv.at</a>'
+          (<a href="https://www.tirol.gv.at/data/nutzungsbedingungen/">CC BY 3.0 AT</a>`,
+      }
+    ),
+    title
+  )
+);
 
 const timeline = L.timeline(Innsbruck, {
   onEachFeature(feature, layer) {
